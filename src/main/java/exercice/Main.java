787b0b990f61c505c1adc99e5857dd77cb995d4c
OozNey <110228768+OozNey@users.noreply.github.com>
2023-06-03 02:01:59 +0200

    test

--- src/main/java/exercice/Main.java
+++ src/main/java/exercice/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] Args) {
         String texte = "exemple de texte a classer";
         List<Character> ordre = List.of('c', 't', 'e', 'x', 'a', 'm', 'p', 'l', 's', 'r', 'd');
-        System.out.println(Exercice.simplicite-meilleur(texte,ordre));
+        System.out.println(exercice.simplicite.trierMots(texte,ordre));
         // output: [classer, texte, exemple, a, de]
     }
 }
